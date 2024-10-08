163abfa586a2eeabcd0b0439147edf57b379b555
dpktjf <43951673+dpktjf@users.noreply.github.com>
2024-10-08 10:57:31 +0000

    update dependencies; remove single threaded as grid now supports multi-connections

--- custom_components/va_scraper/coordinator.py
+++ custom_components/va_scraper/coordinator.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-from asyncio import Lock as Asyncio_lock
-from asyncio import wait_for as asyncio_wait_for
 from typing import TYPE_CHECKING, Any
 
 from homeassistant.helpers.dispatcher import async_dispatcher_send
@@ -34,7 +32,6 @@ class VAScraperDataUpdateCoordinator(DataUpdateCoordinator):
         self.hass = hass
         self._scrape_client = client
         self.data = {}
-        self.lock = Asyncio_lock()
 
         super().__init__(
             hass=hass,
@@ -45,42 +42,11 @@ class VAScraperDataUpdateCoordinator(DataUpdateCoordinator):
 
     async def _async_update_data(self) -> Any:
         """Update data via library."""
-        LOGGER.debug("wait for scraper lock...")
-        try:
-            await asyncio_wait_for(self.lock.acquire(), timeout=10)
-        except Exception:  # noqa: BLE001
-            LOGGER.error("timed out waiting for scraper lock")
-            return None
-        LOGGER.debug("got scraper lock!")
-
         try:
             await self._async_scrape()
         except Exception as error:
-            self.lock.release()
             raise UpdateFailed(error) from error
 
-        self.lock.release()
-        async_dispatcher_send(self.hass, "update_sensors", self)
-        return self.data
-
-    async def _async_update_data_old(self) -> Any:
-        """Update data via library."""
-        LOGGER.debug("wait for scraper lock...")
-        try:
-            await asyncio_wait_for(self.lock.acquire(), timeout=10)
-        except Exception:  # noqa: BLE001
-            LOGGER.error("timed out waiting for scraper lock")
-            return None
-        LOGGER.debug("got scraper lock!")
-
-        try:
-            LOGGER.debug("_async_update_data - calling async_add_executor_job")
-            await self.hass.async_add_executor_job(self.scrape)
-        except Exception as error:
-            self.lock.release()
-            raise UpdateFailed(error) from error
-
-        self.lock.release()
         async_dispatcher_send(self.hass, "update_sensors", self)
         return self.data
 
